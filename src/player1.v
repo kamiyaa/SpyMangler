59cdddd6223dd8f6089cb1597e6b150dbb3f5d31
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-07-18 19:31:26 -0400

    main.v: moved multiple loops into one

--- src/player1.v
+++ src/player1.v
@@ -4,24 +4,33 @@
 `include "morse_decoder.v"
 
 module player1(
-    /* input */
-    clock,          // clock from ps2
-    user_input,     // data from ps2
-    next_input,
-    done_input,
-    resetn          // reset current input
+    /* inputs */
+    clock,
+    user_input,     // user input data
+    next_input,     // user input indicating next char
+    done_input,     // user input indicating end of turn
+    resetn,         // reset current input
+
+    /* outputs */
+    q
     );
 
-    input       clock;
-    input       user_input;
-    input       next_input;
-    input       done_input;
-    input       resetn;
+    input clock;
+    input user_input;
+    input next_input;
+    input done_input;
+    input resetn;
+
+    output [9:0] q;
 
     reg [9:0] p1_value;
 
+    /* finite states */
+    localparam  MORSE_DOT   = 2'b01,
+                MORSE_LINE  = 2'b11 ;
+
     /* module to take in input and convert to morse code */
-    morse_decoder morse0(
+    morse_decoder morse1(
         .clock(clock),
         .user_input(user_input),
         .resetn(resetn),
@@ -34,12 +43,13 @@ module player1(
     always @(posedge clock) begin
         // reset player2 value
 	     if (!resetn)
-		      p1_value <= 0;
+            p1_value <= 0;
         // concatentate dot binary to player2's input value
         if (ld_dot)
-            p1_value <= { p1_value, 2'b01 };
+            p1_value <= { p1_value, MORSE_DOT };
         // concatentate line binary to player2's input value
         if (ld_line)
-            p1_value <= { p1_value, 2'b11 };
+            p1_value <= { p1_value, MORSE_LINE };
     end
+    assign q = p1_value;
 endmodule
--- src/player2.v
+++ src/player2.v
@@ -1,7 +1,7 @@
 // `timescale time_unit/time_precision
 `timescale 1ns / 1ns
 
-`include "morse_decoder.v"
+`include "player1.v"
 
 module player2(
     // input
@@ -10,9 +10,10 @@ module player2(
     next_input, // indicate next input
     done_input, // indicate user is done with input
     resetn,     // reset current input
-    p1_value,
+    p1_value,   // player1's value
 
     // output
+    complete,
     correct,
     q
     );
@@ -24,16 +25,27 @@ module player2(
     input resetn;
     input [9:0] p1_value;
 
-    output correct;
+    output reg correct;
+    output complete;
     output [9:0] q;
 
-    reg [9:0] p2_value;
+    /* states */
     reg [3:0] current_state, next_state;
-
+    /* player2's value */
+    reg [9:0] p2_value;
+    /* indicate whether dot or line */
     wire ld_dot, ld_line;
 
+    /* player1 copy */
+    reg [9:0] p1_copy;
+
+    /* finite states */
+    localparam  MORSE_NONE  = 2'b00,
+                MORSE_DOT   = 2'b01,
+                MORSE_LINE  = 2'b11 ;
+
     /* module to take in input and convert to morse code */
-    morse_decoder morse0(
+    morse_decoder morse2(
         .clock(clock),
         .user_input(user_input),
         .resetn(resetn),
@@ -41,20 +53,40 @@ module player2(
         .ld_line(ld_line)
         );
 
+    wire [1:0] curr_morse;
+    assign curr_morse = p1_copy[9:8];
+
     /* loop to concatentate morse code coming in with
      * existing morse code */
     always @(posedge clock) begin
-        // reset player2 value
-	     if (!resetn)
-		      p2_value <= 0;
+        correct <= 0;
+        // morse value is empty
+        if (curr_morse == 2'b00) begin
+            correct <= 1;
+            p1_copy <= p1_copy << 2;
+        end
         // concatentate dot binary to player2's input value
-        if (ld_dot)
-            p2_value <= { p2_value, 2'b01 };
+        if (ld_dot) begin
+            p2_value <= { p2_value, MORSE_DOT };
+            if (curr_morse == MORSE_DOT) begin
+                correct <= 1;
+            end
+            p1_copy <= p1_copy << 2;
+        end
         // concatentate line binary to player2's input value
-        if (ld_line)
-            p2_value <= { p2_value, 2'b11 };
+        if (ld_line) begin
+            p2_value <= { p2_value, MORSE_LINE };
+            if (curr_morse == MORSE_DOT) begin
+                correct <= 1;
+            end
+            p1_copy <= p1_copy << 2;
+        end
+        // reset player2 value if incorrect
+	    if (!correct) begin
+            p1_copy <= p1_value;
+            p2_value <= 0;
+        end
     end
-    // assign correct a value when player click finish
-    assign correct = done_input ? 1'b0 : (p2_value == p1_value);
+    assign complete = (p2_value == p1_value);
     assign q = p2_value;
 endmodule
