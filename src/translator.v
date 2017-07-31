f179a66afb8c5844142793de8e05e48d0d082f61
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-07-31 19:26:58 -0400

    add comments to translator.v

--- src/translator.v
+++ src/translator.v
@@ -10,39 +10,49 @@ module translator(correct, columns, selection, X, Y, colour, draw_full, reset);
 	reg [4:0] row, column;
 	
 	always @(*) begin
+		// coordinates the positions of squares as position*spacing+offset
 		X <= (column*11)+28;
 		Y <= (row*8)+30;
 	end
 
+	// delays the signal to coordinate with main code
 	wire signal;
 	assign signal = (correct != 2'b00);
 	reg [1:0] correct_reg;
 
+	// on signal evaluate where to draw the next square
 	always @(posedge signal, negedge reset) begin
 		correct_reg <= correct;
 		if (reset == 1'b0) begin 
+			// to reset revert all columns and rows to starting positions
 			column <= 0;
 			row <= 0;
 		end
 		else if (correct_reg == 2'b01 && row == 5'b00100) begin
+			// if correct and we reached the end, move to top of next column
 			row <= 5'b0;
 			column <= column + 1'b1;
 		end
 		else if (correct_reg == 2'b01)
+			// otherwise, correct results lower the square
 			row <= row + 1'b1;
 		else
+			// otherwise, incorrect results places the square to the top of column
 			row <= 5'b0;
 	end
 	always @(*) begin
 		if (selection == 2'b00) begin
+			// if we are drawing a 00 blank, we draw a filled red square
 			colour <= 3'b100;
 			draw_full <= 1'b1;
 		end
 		else if (selection == 2'b11) begin
+			// if we are drawing a 11 dash, we draw a hoolow white square
 			colour <= 3'b111;
 			draw_full <= 1'b0;
 		end
 		else if (selection == 2'b01) begin
+			// if we are drawing a 01 blank, we draw a filled white square
 			colour <= 3'b111;
 			draw_full <= 1'b1;
 		end
